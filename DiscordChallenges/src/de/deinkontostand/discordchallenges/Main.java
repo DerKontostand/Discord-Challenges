90d5b6546a7295f3c917283f7b5d888df5116055
PHILGAMER <73665072+PHILGAMER@users.noreply.github.com>
2021-05-16 12:59:02 +0200

    Update Main.java

--- DiscordChallenges/src/de/deinkontostand/discordchallenges/Main.java
+++ DiscordChallenges/src/de/deinkontostand/discordchallenges/Main.java
@@ -15,6 +15,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
 
     public static Main plugin;
+    
+    public static Main plugin;
+    public static int time = 0;
 
     public static boolean mobdamageswitch = false;
     public static boolean itemdamageswitch = false;
@@ -30,6 +33,7 @@ public class Main extends JavaPlugin {
     public void onEnable() {
 
         getCommand("settings").setExecutor(new openInventory());
+        getCommand("timer").setExecutor(new TimerCMD());
 
         Bukkit.getPluginManager().registerEvents(new WeaknessMobKill(), this);
         Bukkit.getPluginManager().registerEvents(new MobDamageSwitch(), this);
@@ -39,10 +43,77 @@ public class Main extends JavaPlugin {
         Bukkit.getPluginManager().registerEvents(new WithoutInv(), this);
         Bukkit.getPluginManager().registerEvents(new NoInvDrop(), this);
 
+                new TimerScheduler().runTaskTimer(getPlugin(), 20L, 20L);
+
+        getConfig().options().copyDefaults(true);
+        saveConfig();
 
 
 
     }
+    
+    public static Main getPlugin() {
+        return plugin;
+    }
+
+
+
+    public BukkitRunnable getRunnable() {
+        return runnable;
+    }
+
+    public static void TimerModule(){
+        runnable = new BukkitRunnable() {
+            @Override
+            public void run() {
+                Bukkit.getOnlinePlayers().forEach(player -> {
+                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent("§6§l" + shortInteger(Main.time)));
+                });
+
+
+                Main.time++;
+            }
+        };
+        runnable.runTaskTimer(Main.getPlugin(), 0, 20);
+
+    }
+
+    public static String shortInteger(int duration) {
+        String string = "";
+        int hours = 0;
+        int minutes = 0;
+        int seconds = 0;
+        if (duration / 60 / 60 / 24 >= 1) {
+            duration -= duration / 60 / 60 / 24 * 60 * 60 * 24;
+        }
+        if (duration / 60 / 60 >= 1) {
+            hours = duration / 60 / 60;
+            duration -= duration / 60 / 60 * 60 * 60;
+        }
+        if (duration / 60 >= 1) {
+            minutes = duration / 60;
+            duration -= duration / 60 * 60;
+        }
+        if (duration >= 1)
+            seconds = duration;
+        if (hours <= 9) {
+            string = String.valueOf(string) + "0" + hours + "§7§l:§6§l";
+        } else {
+            string = String.valueOf(string) + hours + "§7§l:§6§l";
+        }
+        if (minutes <= 9) {
+            string = String.valueOf(string) + "0" + minutes + "§7§l:§6§l";
+        } else {
+            string = String.valueOf(string) + minutes + "§7§l:§6§l";
+        }
+        if (seconds <= 9) {
+            string = String.valueOf(string) + "0" + seconds;
+        } else {
+            string = String.valueOf(string) + seconds;
+        }
+        return string;
+    }
+
 
     public static void help(Player p){
 
@@ -59,4 +130,17 @@ public class Main extends JavaPlugin {
     public static Main getPlugin() {
         return plugin;
     }
+    public static void stopchallenge(){
+            Bukkit.getOnlinePlayers().forEach(players ->{
+            players.setGameMode(GameMode.SPECTATOR);
+            Main.getPlugin().getRunnable().cancel();
+            Timer = false;
+
+            new TimerScheduler().runTaskTimer(Main.getPlugin(), 20L, 20L);
+        });
+
+    }
+
+
+
 }
